fab1a69d5eb75fdd17fb1636f5046802bbee934b
mlabenski <mlabenski123@gmail.com>
2019-02-23 18:25:13 -0500

    changed colors

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  template: `<h1>Hey {{this.specialName}} , do you need some loving?</h1>`,
+  template: `<h1 style="color: #F63F75">Hey {{this.specialName}} , do you need some loving?</h1>`,
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
@@ -16,7 +16,6 @@ export class HeaderComponent implements OnInit {
   constructor() { 
     this.specialNameArray = ['babe', 'madi', 'girl'];
     // Going to need to fix this array with a random index
-    this.specialName = this.specialNameArray[1];
     this.specialName = this.specialNameArray[Math.floor(Math.random() * this.specialNameArray.length)];
     this.headingName = "Hey "+ this.specialName + ", do you need some loving?";
     console.log(this.headingName);
